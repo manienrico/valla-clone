4b77450110f7243508ec25cf3b8f4d2b38c6ba3e
Enrico Mani <manienrico13@gmail.com>
2024-03-02 20:08:08 +0300

    Features section populating

--- src/sections/Features/features.section.jsx
+++ src/sections/Features/features.section.jsx
@@ -1,7 +1,13 @@
+import Feature from '../../components/feature/feature.component'
 import './feature.section.css'
 
 export default function Features() {
   return (
-    <div>Features</div>
+    <section>
+      <Feature containerName='' picUrl='' picAlt='' picClassName='' featureWords='Encrypted payment credentials' cFeature='' />
+      <Feature containerName='' picUrl='' picAlt='' picClassName='' featureWords='QR code payments' cFeature='' />
+      <Feature containerName='' picUrl='' picAlt='' picClassName='' featureWords='Reference code payments' cFeature='' />
+      <Feature containerName='' picUrl='' picAlt='' picClassName='' featureWords='In-App budgeting' cFeature='' />
+    </section>
   )
 }
