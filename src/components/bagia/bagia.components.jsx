2c20b3b7b688f58ef8e89443e3ea2d4ad12871eb
Enrico Mani <manienrico13@gmail.com>
2024-03-02 20:48:54 +0300

    footer and credits structure, credit structure still basic

--- src/components/bagia/bagia.components.jsx
+++ src/components/bagia/bagia.components.jsx
@@ -1,8 +1,8 @@
-export default function Bagia() {
+export default function Bagia({bagiaContainer,cHead,ulContainer}) {
   return (
-    <div>
-        <span>Valla</span>
-        <ul>
+    <div className={bagiaContainer}>
+        <span className={cHead}>Valla</span>
+        <ul className={ulContainer}>
             <li><a href="#">X</a></li>
             <li><a href="#">Linkedin</a></li>
             <li><a href="#">instagram</a></li>
---
+++ src/components/credits/credits.components.jsx
@@ -0,0 +1,12 @@
+import './credits.component.css'
+
+export default function Credits() {
+  return (
+    <ul>
+        <li><a href='#'>{}</a></li>
+        <li><a href='#'>{}</a></li>
+        <li><a href='#'>{}</a></li>
+        <li><a href='#'>{}</a></li>
+    </ul>
+  )
+}
