21374ddae729949fa55bc9cd0bc9045b2d849b6e
Enrico Mani <manienrico13@gmail.com>
2024-03-10 14:01:45 +0300

    added icons to btn in nav

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,6 +1,6 @@
 import './button.component.css'
 
-export default function Button({name,type,className,btnLink,clicked,icon}) {
+export default function Button({name,type,className,btnLink,clicked}) {
   return (
     <button onClick={clicked} className={className} type={type}>
       <a href={btnLink}>{name}</a>
--- src/sections/Navigation/Nav.component.jsx
+++ src/sections/Navigation/Nav.component.jsx
@@ -1,4 +1,4 @@
-import { GoPerson } from "react-icons/go";
+import { GoPerson,GoSignIn } from "react-icons/go";
 import Button from "../../components/button/button.component"
 import "./nav.component.css"
 
@@ -7,10 +7,8 @@ function Nav() {
     <nav>
         <h3 className="lnav">Val<span className="logo">la</span></h3>
         <div className="rnav">
-        <Button name='Profile' btnLink='#' className='profile' />
-        <Button name='Sign In' btnLink='#' className='signin' />
-            {/* <div className="profile btn"><a href="#">Profile</a></div>
-            <div className="signin btn"><a href="#">Sign In</a></div> */}
+        <span><GoPerson /><Button name='Profile' btnLink='#' className='profile' /></span>
+        <span><GoSignIn /><Button name='Sign In' btnLink='#' className='signin' /></span>
         </div>
     </nav>
   )
