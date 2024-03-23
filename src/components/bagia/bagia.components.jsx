dcfe99453d50c223c991f3bddd9f23259f195fa9
Enrico Mani <manienrico13@gmail.com>
2024-03-23 21:59:43 +0300

    social icon imgs added

--- src/components/bagia/bagia.components.jsx
+++ src/components/bagia/bagia.components.jsx
@@ -1,11 +1,15 @@
+import { RiTwitterXFill } from "react-icons/ri";
+import { FiLinkedin } from "react-icons/fi";
+import { IoLogoInstagram } from "react-icons/io";
+
 export default function Bagia({bagiaContainer,cHead,ulContainer}) {
   return (
     <div className={bagiaContainer}>
         <span className={cHead}>Valla</span>
         <ul className={ulContainer}>
-            <li><a href="#">X</a></li>
-            <li><a href="#">Linkedin</a></li>
-            <li><a href="#">instagram</a></li>
+            <li><a href="#"><RiTwitterXFill /></a></li>
+            <li><a href="#"><FiLinkedin /></a></li>
+            <li><a href="#"><IoLogoInstagram /></a></li>
         </ul>
     </div>
   )
