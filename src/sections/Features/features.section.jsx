75cc5167a88921aee1640cf89cdfeac9414bc3d9
Enrico Mani <manienrico13@gmail.com>
2024-03-24 11:50:47 +0300

    feature default img addition, abit of content

--- src/sections/Features/features.section.jsx
+++ src/sections/Features/features.section.jsx
@@ -1,14 +1,15 @@
 import { AiOutlineSafety } from "react-icons/ai";
 import Feature from '../../components/feature/feature.component'
 import './features.section.css'
+import img from '../../assests/img-2.jpg'
 
 export default function Features() {
   return (
     <section>
-      <Feature containerName='a' picUrl='' picAlt='' picClassName='' featureWords='Encrypted payment credentials' cFeature='' />
-      <Feature containerName='a' picUrl='' picAlt='' picClassName='' featureWords='QR code payments' cFeature='' />
-      <Feature containerName='a' picUrl='' picAlt='' picClassName='' featureWords='Reference code payments' cFeature='' />
-      <Feature containerName='a' picUrl='' picAlt='' picClassName='' featureWords='In-App budgeting' cFeature='' />
-    </section>
+      <div>
+        <Feature picUrl={img} />
+      </div>
+      <div></div>
+      </section>
   )
 }
