d0bc6f86cdf4553a8da8204fb3c672693f38d02d
Enrico Mani <manienrico13@gmail.com>
2024-03-28 13:26:59 +0300

    partner section structure

--- src/sections/Features/features.section.jsx
+++ src/sections/Features/features.section.jsx
@@ -1,4 +1,7 @@
-import  LuShieldCheck  from "react-icons/lu";
+import  {LuShieldCheck}  from "react-icons/lu";
+import { LuQrCode } from "react-icons/lu";
+import { IoReceiptOutline } from "react-icons/io5";
+import { BsPhone } from "react-icons/bs";
 import { AiOutlineSafety } from "react-icons/ai";
 import Feature from '../../components/feature/feature.component'
 import './features.section.css'
@@ -6,15 +9,15 @@ import img from '../../assests/img-2.jpg'
 
 export default function Features() {
   return (
-    <section>
-      <div>
-        <Feature picUrl={img} />
+    <section id="feature">
+      <div className="fLeft">
+        <Feature picUrl={img} picClassName='fpic' />
       </div>
-      <div>
-        <Feature containerName='a' picUrl='' picAlt='' picClassName='' featureWords='Encrypted payment credentials' cFeature='' />
-        <Feature containerName='a' picUrl='' picAlt='' picClassName='' featureWords='QR code payments' cFeature='' />
-        <Feature containerName='a' picUrl='' picAlt='' picClassName='' featureWords='Reference code payments' cFeature='' />
-        <Feature containerName='a' picUrl='' picAlt='' picClassName='' featureWords='In-App budgeting' cFeature='' />
+      <div className="fRight">
+        <p id="feat"><LuShieldCheck /><Feature containerName='a' picUrl='' picAlt='' picClassName='' featureWords='Encrypted payment credentials' cFeature='' /></p>
+        <p id="feat"><LuQrCode /><Feature containerName='a' picUrl='' picAlt='' picClassName='' featureWords='QR code payments' cFeature='' /></p>
+        <p id="feat"><IoReceiptOutline /><Feature containerName='a' picUrl='' picAlt='' picClassName='' featureWords='Reference code payments' cFeature='' /></p>
+        <p id="feat"><BsPhone /><Feature containerName='a' picUrl='' picAlt='' picClassName='' featureWords='In-App budgeting' cFeature='' /></p>
       </div>
       </section>
   )
---
+++ src/sections/Partners/Partners.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Partners() {
+  return (
+    <section>
+        <h3>Supported payment options</h3>
+    </section>
+  )
+}
