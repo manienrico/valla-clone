de3409e8a1fe37ac9c5c4c0157d71ad8ffe35aa1
Enrico Mani <manienrico13@gmail.com>
2024-03-29 15:14:10 +0300

    vercel speedinsights added

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 //import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App >
+      <SpeedInsights />
+    </App>
   </React.StrictMode>
 );
